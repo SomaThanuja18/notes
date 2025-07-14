739bf07e25771f817435a8379155d120d82fc8a4
SomaThanuja18 <229x1a0524@gprec.ac.in>
2025-07-14 21:09:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 import authRoutes from './routes/auth.js';
 import labelRoutes from './routes/labels.js';
@@ -15,6 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/labels', labelRoutes);
@@ -22,12 +26,31 @@ app.use('/api/notes', noteRoutes);
 app.use('/api/user', userRoutes);
 
 // Direct MongoDB URI here (no .env)
-mongoose.connect('mongodb://localhost:27017/thanuja-notes-app')
-  .then(() => {
+// mongoose.connect(process.env.MONGOURI)
+//   .then(() => {
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log(`Backend running on http://localhost:${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error('Failed to connect to MongoDB:', err.message);
+//   });
+
+
+  const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://babunkundu60:0EEN18UaPa93Fk91@stminorg27.plp450e.mongodb.net/thanuja-notes-app?retryWrites=true&w=majority&appName=stminorg27'
+    );
+    console.log('Connected to MongoDB');
+
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err.message);
+  }
+};
+
+connectToDatabase();
+
     app.listen(5000, () => {
-      console.log(' Backend running on http://localhost:5000');
+      console.log('Backend running on http://localhost:5000');
     });
-  })
-  .catch((err) => {
-    console.error(' Failed to connect to MongoDB:', err.message);
-  });
